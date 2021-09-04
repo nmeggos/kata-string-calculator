c7879a1bdf543c512207a466e7dab7b75cdce0c3
Nikitas Meggos <nmeggos@visualcomfort.com>
2021-09-04 06:04:03 -0500

    adding tests for checking null, empty or whitespace and returning 0 adding tests for when single integer value provided returns expected integer

--- dotnetcore/tests/StringCalculator.UnitTests/StringCalculatorShould.cs
+++ dotnetcore/tests/StringCalculator.UnitTests/StringCalculatorShould.cs
@@ -7,11 +7,49 @@ namespace KataStringCalculator.UnitTests
 {
     public class StringCalculatorShould
     {
-        [Fact]
-        public void ReturnZeroWhenStringValueIsNullOrWhiteSpace()
+        [Fact(DisplayName = "Return Zero When String Value Is Null")]
+        public void ReturnZeroWhenStringValueIsNull()
         {
             string value = null;
             int expected = 0;
+
+            var sut = StringCalculator.Add(value);
+
+            sut.Should().Equals(expected);
+        }
+
+        [Fact(DisplayName = "Return Zero When String Value Is Empty")]
+        public void ReturnZeroWhenStringValueIsEmpty()
+        {
+            string value = String.Empty;
+            int expected = 0;
+
+            var sut = StringCalculator.Add(value);
+
+            sut.Should().Equals(expected);
+
+            value = "";
+            sut = StringCalculator.Add(value);
+            
+            sut.Should().Equals(expected);
+        }
+
+        [Fact(DisplayName = "Return Zero When String Value Is White Space")]
+        public void ReturnZeroWhenStringValueIsWhiteSpace()
+        {
+            string value = " ";
+            int expected = 0;
+
+            var sut = StringCalculator.Add(value);
+            
+            sut.Should().Equals(expected);
+        }
+
+        [Fact(DisplayName = "Return Integer Value When One Integer Value Is Provided")]
+        public void ReturnIntegerValueWhenOneIntegerValueIsProvided()
+        {
+            string value = "1";
+            int expected = 1;
 
             var sut = StringCalculator.Add(value);
 
